f3ad34c84bf8ddd9642e517d5014c4d35ce908a1
dezeidler <dezeidler@gmail.com>
2016-10-09 14:26:02 -0400

    Add files via upload

---
+++ ard3/ard3/ard3.ino
@@ -0,0 +1,29 @@
+/////Universal/////
+int To;
+int T;
+/////
+
+/////ACCELORATION/////
+int Ao;         //Rest Acceloration Read
+int AccConv;   //Converertion factor from analog in to acceloration
+int AccPin=0;   //Acceloration read from analogIn 1
+int Ax;         //Acceloration Value
+int vel;        //velocity
+int pos;        //position
+/////
+
+
+void setup() {
+  Serial.begin(9600);
+  Ao = analogRead(AccPin);
+  vel=0, pos=0, To=0, T=0;  
+}
+
+void loop() {
+  To=millis();
+  Ax = (analogRead(AccPin) - Ao)/AccConv;    //current Acc
+  vel+=Ax * T;
+  pos+=vel * T;
+  T=millis()-To;
+
+}
