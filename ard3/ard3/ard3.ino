ba549110a4b220403c2585151b940403c91f351e
dezeidler <dezeidler@gmail.com>
2016-10-09 14:29:18 -0400

    Update ard3.ino

--- ard3/ard3/ard3.ino
+++ ard3/ard3/ard3.ino
@@ -12,16 +12,28 @@ int vel;        //velocity
 int pos;        //position
 /////
 
+/////Temperature/////
+int Temp;
+int TempPin=1;
+/////
+
+/////Pressure/////
+int Press;
+int PressPin=2;
+/////
+
 
 void setup() {
   Serial.begin(9600);
+  
   Ao = analogRead(AccPin);
   vel=0, pos=0, To=0, T=0;  
+  AccConv=0;
 }
 
 void loop() {
   To=millis();
-  Ax = (analogRead(AccPin) - Ao)/AccConv;    //current Acc
+  Ax = (analogRead(AccPin) - Ao);//      /AccConv;    //current Acc
   vel+=Ax * T;
   pos+=vel * T;
   T=millis()-To;
